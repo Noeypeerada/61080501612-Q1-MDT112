01d91a1204920f5b3f72d872e262bb10a9ac87ab
noeypeerada <Noeypeerada12@gmail.com>
2020-04-22 23:41:37 +0700

    Q1

---
+++ 61080501612-Q1-MDT112.ino
@@ -0,0 +1,138 @@
+void setup()
+{
+
+    //start serial connection
+
+    Serial.begin(9600); 
+
+    pinMode(2,INPUT_PULLUP); 
+
+    pinMode(3,OUTPUT);
+
+    pinMode(4,OUTPUT);
+
+    pinMode(5,OUTPUT);
+
+    pinMode(6,OUTPUT);
+
+    pinMode(7,OUTPUT);
+
+    pinMode(8,OUTPUT);
+
+    pinMode(9,OUTPUT);
+
+    pinMode(10,OUTPUT);
+
+    pinMode(11,OUTPUT);
+
+    pinMode(12,OUTPUT);
+
+    pinMode(13,OUTPUT);
+
+     for(int light = 3 ; light <= 13 ;light++)
+     {
+
+        pinMode(light,OUTPUT);
+
+    }
+
+    tone(8,200,500); 
+    
+    delay(250); 
+
+    tone(8,400,100); 
+
+}
+
+int bright = 3;
+
+int time = 100;
+
+int press = 1;
+
+void loop()
+
+{
+
+    for(bright = 3 ; bright <= 13 ; bright++) 
+    { 
+
+        if(bright == 8)
+        {
+
+            digitalWrite(bright,0);
+
+            continue;
+
+        }
+
+        digitalWrite(bright,1); 
+
+        delay(time);
+
+        digitalWrite(bright,0); 
+
+        if ( digitalRead(2) == 0 ){
+
+            press = press * 2;
+
+            time = time / 2;
+
+            Serial.println("Faster X 2 (Current Speed : X" + String(press) + ")");
+
+            tone(8,400,100);
+
+            delay(500);
+
+        }
+
+        else  
+        {
+
+        }
+
+    }
+
+    for( bright= 13 ; bright >= 3 ;bright--) // ลง
+
+    {
+
+        if(bright == 8)
+        {
+
+            digitalWrite(bright,0); // เปิดปิด L8
+
+            continue;
+
+        }
+
+        digitalWrite(bright,1); // เปิด
+
+        delay(time);
+
+        digitalWrite(bright,0); // ปิด
+
+        if ( digitalRead(2) == 0 )
+        {
+
+            press = press * 2;
+
+            Serial.println("Faster X 2 (Current Speed : X" + String(press) + ")");
+
+            tone(8,400,100);
+
+            time = time / 2;
+
+            delay(500);
+ 
+        }
+
+        else  
+
+        {
+
+        }
+
+    }
+
+}
